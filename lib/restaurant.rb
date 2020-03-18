c5f23432df420497a79b411dffa4d5a06b59f861
stellakunzang <stella.k.bonnie@gmail.com>
2020-03-18 13:50:25 -0600

    Add tests and methods

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,1 +1,40 @@
+class Restaurant
+attr_reader :opening_time, :name, :dishes
 
+  def initialize(opening_time, name)
+    @opening_time = opening_time
+    @name = name
+    @dishes = []
+  end
+
+  def closing_time(hours_open)
+    integer_hours = @opening_time.to_i + hours_open
+    "#{integer_hours.to_s}:00"
+  end
+
+  def add_dish(dish_name)
+    @dishes << dish_name
+  end
+
+  def open_for_lunch
+    true if @opening_time.to_i < 12
+  end
+
+  def menu_dish_names
+    excited_dishes = []
+    @dishes.each do |dish|
+      excited_dishes << dish.upcase
+    end
+    excited_dishes
+  end
+
+  def announce_closing_time(hours_open)
+    time = @opening_time.to_i + hours_open
+    if time < 12
+      return "#{@name} will be closing at #{time}:00AM"
+    else
+      pm_hours = time.to_i - 12
+      return "#{@name} will be closing at #{pm_hours.to_s}:00PM"
+    end
+  end
+end
--- test/restaurant_test.rb
+++ test/restaurant_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/restaurant'
+require '../lib/restaurant'
+require 'pry'
 
 class RestaurantTest < Minitest::Test
   # Iteration 1 Tests:
@@ -11,21 +12,18 @@ class RestaurantTest < Minitest::Test
   end
 
   def test_it_has_opening_time
-    skip
     restaurant = Restaurant.new('10:00', 'Fuel Cafe')
 
     assert_equal '10:00', restaurant.opening_time
   end
 
   def test_it_has_name
-    skip
     restaurant = Restaurant.new('10:00', 'Fuel Cafe')
 
     assert_equal 'Fuel Cafe', restaurant.name
   end
 
   def test_it_has_dishes
-    skip
     restaurant = Restaurant.new('10:00', 'Fuel Cafe')
 
     assert_equal [], restaurant.dishes
@@ -33,16 +31,15 @@ class RestaurantTest < Minitest::Test
 
   #Iteration 2 Tests:
   def test_it_has_closing_time
-    skip
     restaurant1 = Restaurant.new('10:00', 'Fuel Cafe')
     restaurant2 = Restaurant.new('16:00', 'Il Poggio')
+
 
     assert_equal '18:00', restaurant1.closing_time(8)
     assert_equal '23:00', restaurant2.closing_time(7)
   end
 
   def test_it_can_add_dishes
-    skip
     restaurant = Restaurant.new('16:00', 'Il Poggio')
 
     restaurant.add_dish('Burrata')
@@ -50,5 +47,35 @@ class RestaurantTest < Minitest::Test
     restaurant.add_dish('Ravioli')
 
     assert_equal ['Burrata', 'Pizzetta', 'Ravioli'], restaurant.dishes
+  end
+
+  def test_it_can_open_for_lunch
+    restaurant1 = Restaurant.new('10:00', 'Fuel Cafe')
+    assert restaurant1.open_for_lunch
+  end
+
+  def test_it_can_close_for_lunch
+    restaurant2 = Restaurant.new('16:00', 'Il Posto')
+    refute restaurant2.open_for_lunch
+  end
+
+  def test_it_can_be_excited_about_menu_dish_names
+    restaurant = Restaurant.new('16:00', 'Il Poggio')
+
+    restaurant.add_dish('Burrata')
+    restaurant.add_dish('Pizzetta')
+    restaurant.add_dish('Ravioli')
+
+    assert_equal ["BURRATA", "PIZZETTA", "RAVIOLI"], restaurant.menu_dish_names
+  end
+
+  def test_it_can_accounce_closing_time_24_hour_clock
+    restaurant1 = Restaurant.new('6:00', 'Fuel Cafe')
+    assert_equal "Fuel Cafe will be closing at 11:00AM", restaurant1.announce_closing_time(5)
+  end
+
+  def test_it_can_accounce_closing_time_12_hour_clock
+    restaurant2 = Restaurant.new('16:00', 'Il Posto')
+    assert_equal "Il Posto will be closing at 11:00PM", restaurant2.announce_closing_time(7)
   end
 end
